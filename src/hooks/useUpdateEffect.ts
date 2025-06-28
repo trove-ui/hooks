df96dd026886ac5b785bfda7be717ea631e84082
Your Name <you@example.com>
2025-06-28 13:02:12 +0800

    feat(utils): 添加 sleep 工具函数用于延迟执行
    feat(hooks): 添加 useUpdateEffect 钩子仅在依赖更新时执行

---
+++ src/hooks/useUpdateEffect.ts
@@ -0,0 +1,13 @@
+import { useEffect, useRef, type DependencyList, type EffectCallback } from "react";
+
+export const useUpdateEffect = (effect: EffectCallback, deps?: DependencyList) => {
+  const isFirst = useRef(false);
+
+  useEffect(() => {
+    if (!isFirst.current) {
+      isFirst.current = true;
+      return;
+    }
+    return effect();
+  }, deps);
+}
---
+++ src/utils/sleep.ts
@@ -0,0 +1,8 @@
+/**
+ * 休眠指定的时间之后执行一些操作
+ * @param ms 延迟的时间 单位：毫秒
+ * @returns 
+ */
+export const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
